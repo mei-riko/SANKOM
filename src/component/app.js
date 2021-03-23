8ef69b07122454189490449d2719a5124c18a7a0
mei-riko <dobrovolska.marina@gmail.com>
2021-03-23 14:33:08 +0300

    catagory & start catalog

--- src/component/app.js
+++ src/component/app.js
@@ -20,4 +20,39 @@ $(document).ready(() =>{
   $('a[data-trigger="click"]').on('click', function(e){
     e.preventDefault();
   })
+  // Input
+  const addFocusListener = (elem) => {
+    console.log ( elem );
+    elem.addEventListener('focus', function() {
+      elem.classList.add('input__field--focus');
+    });
+    elem.addEventListener('blur', function() {
+      if( elem.value.length === 0 ){
+        elem.classList.remove('input__field--focus');
+      }
+    });
+  }
+
+  let inputs = $('.input__field');
+  for(let i = 0; i < inputs.length; i++) {
+    addFocusListener( inputs[i] );
+  }
+
+  // Open More Content
+  const openContent = ( block ) => {
+    let blockId = block.data( 'id' );
+    if ( !block.hasClass('active') ){
+      block.addClass('active');
+      block.text('Скрыть подробности')
+    }else{
+      block.removeClass('active');
+      block.text('Читать дальше >')
+    }
+
+    $('[data-content=' + blockId + ']').slideToggle();
+  }
+  $('.open-content').on('click', function(){
+    openContent( $(this) );
+  });
+  
 });
