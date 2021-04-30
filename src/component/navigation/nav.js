4e44e5a9b6e3316a6fc46f746beaace25f1bd152
mei-riko <dobrovolska.marina@gmail.com>
2021-04-30 12:23:12 +0300

    new nav

--- src/component/navigation/nav.js
+++ src/component/navigation/nav.js
@@ -21,21 +21,32 @@ function navbarUnHover(){
     item.removeClass("navbar__link--active");
     $(".navbar .navbar__children" + nav).removeClass("navbar__children--active");
 }
+
 // Mobile Navbar
 function openNavbarOnClick( elem ){
     const idNavbar = elem.data('navbar');
     const target = $('.navbar-sidebar#' + idNavbar);
 
-    console.log( idNavbar + "  " + target );
+    // Если есть открытое меню - скрыть его
+    $(".navbar-sidebar").removeClass("navbar-sidebar--active");
 
     if ( target.length > 0 ){
         target.addClass('navbar-sidebar--active');
         $('.overlay').removeClass('overlay--disable');
         $('body').addClass('hidden');
+        $('body').addClass('open-navbar');
     } else{
         return null;
     }
 }
+export function closeNavbarOnClick(){
+    $(".navbar-sidebar").removeClass("navbar-sidebar--active");
+    $(".navbar-mobile .navbar-mobile__item").removeClass("navbar-mobile__item--active");
+
+    $('.overlay').addClass('overlay--disable');
+    $('body').removeClass('hidden');
+    $('body').removeClass('open-navbar');
+};
 
 $(function(){
     // Check Windows Size
@@ -61,54 +72,77 @@ $(function(){
             timeout = setTimeout( navbarUnHover , 300);
         });
 
-    // Mobile Sidebar Nav Show
-    $('[data-navbar]').on('click', function( event ){
+    // Mobile Sidebar Nav
+    $("[data-navbar]").on("click", function( event ){
         event.preventDefault();
+
         if( !$(this).hasClass('navbar-mobile__item--active') ){
+            if( $('body').hasClass('open-sidebar') ){
+                $(".sidebar.sidebar--active").removeClass('sidebar--active');
+                $('body').removeClass('open-sidebar');
+            }
             openNavbarOnClick( $(this) );
-            $(this).addClass('navbar-mobile__item--active');
+            // Убрать активные классы у кнопок, кроме той, на которую нажали
+            $(this).parent().find(".navbar-mobile__item").removeClass("navbar-mobile__item--active").eq($(this).index()).addClass("navbar-mobile__item--active");
         }else{
-            $(this).removeClass('navbar-mobile__item--active');
-            $(".navbar-sidebar").removeClass("navbar-sidebar--active");
-            $('.overlay').addClass('overlay--disable');
-            $('body').removeClass('hidden');
+            closeNavbarOnClick();
         }
     });
-    // Mobile Nav Open Children
-    $('.navbar_mobile .navbar_mobile__parent .navbar_mobile__parent-icon').on('click', function(){
+
+    // Mobile Nav Slide
+    $('.navbar_mobile .navbar_mobile__parent .navbar_mobile__parent-link').on("click", function(e){
+        e.preventDefault();
+
         const parent = $(this).parent();
-        const children = parent.find('.navbar_mobile__children');
+        const title = $(this);
+        const icon = $(this).find('.navbar_mobile__parent-icon');
+        const children = $('.navbar_mobile__children#' + icon.data('nav') );
+
         const parentActive = $('.navbar_mobile .navbar_mobile__parent.navbar_mobile__parent--active');
         
         if( !parent.hasClass('navbar_mobile__parent--active') ){
             parent.addClass('navbar_mobile__parent--active');
+            icon.addClass('navbar_mobile__parent-icon--active');
+            title.addClass('navbar_mobile__parent-link--active');
+
             children.show();
             
             if( parentActive.length > 0 && parent !=  parentActive){
                 parentActive.removeClass('navbar_mobile__parent--active');
-                parentActive.find('.navbar_mobile__children').hide();
+                parentActive.find('.navbar_mobile__parent-link').removeClass('navbar_mobile__parent-link--active');
+                parentActive.find('.navbar_mobile__parent-icon').removeClass('navbar_mobile__parent-icon--active');
+                
+                let childrenActiveId = parentActive.find('.navbar_mobile__parent-icon').data('nav');
+                parentActive.find('.navbar_mobile__children#' + childrenActiveId).hide();
             }
         }else{
             parent.removeClass('navbar_mobile__parent--active');
+            icon.removeClass('navbar_mobile__parent-icon--active');
+            title.removeClass('navbar_mobile__parent-link--active');
+
             children.hide();
         }
 
-        // console.log( parent.prepend( '<span>' + parent.offset().top + '</span>') );
-
         if( parent.offset().top > 75 || parent.offset().top < 0 ){
-            parent.get(0).scrollIntoView(75);
+            parent.get(0).scrollIntoView();
         }
     });
+});
+
+$('.navbar-sidebar .navbar-sidebar__close').on("click", function(){
+    closeNavbarOnClick();
+    // $(this).closest('.navbar-sidebar').removeClass('navbar-sidebar--active');
+    // $('.overlay').addClass('overlay--disable');
+    // $('body').removeClass('hidden');
+    // $('body').removeClass('open-navbar');
 });
 
 // Resize
 $(window).on("resize", function(){
     if ( $(window).width() > 991 || !window.matchMedia('screen and (max-width: 992px)').matches ){
-        // Hide Navigation Sidebar on Desktop
-        $(".navbar-sidebar").removeClass("navbar-sidebar--active");
-        $('.overlay').addClass('overlay--disable');
-        $('body').removeClass('hidden');
-        // Hide 
+        // Close Navigation Sidebar on Desktop
+        closeNavbarOnClick();
+        // Hide Open Children Nav
         const navbarActive = $(".navbar_mobile .navbar_mobile__parent.navbar_mobile__parent--active");
         navbarActive.find('.navbar_mobile__children').hide();
         navbarActive.removeClass("navbar_mobile__parent--active");
@@ -125,9 +159,12 @@ $(window).on("resize", function(){
 $(document).on('mouseup', function (e){ 
     let navbarActive = $(".navbar-sidebar.navbar-sidebar--active"); // элемент
     let navbarItem = $(".navbar-mobile .navbar-mobile__item");
+
+
     if (!navbarActive.is(e.target) // клик был не по блоку
           && navbarActive.has(e.target).length === 0 // и не по его дочерним элементам
           && !navbarItem.is(e.target)
+          && $('body').hasClass('open-navbar')
     ) { 
         navbarActive.removeClass('navbar-sidebar--active');
         $('.overlay').addClass('overlay--disable');
--- src/component/sidebar/sidebar.js
+++ src/component/sidebar/sidebar.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import {closeNavbarOnClick} from '../navigation/nav';
 
 function openSidebarOnClick( elem ){
     const idSidebar = elem.data('sidebar');
@@ -8,20 +9,32 @@ function openSidebarOnClick( elem ){
         target.addClass('sidebar--active');
         $('.overlay').removeClass('overlay--disable');
         $('body').addClass('hidden');
+        $('body').addClass('open-sidebar');
     } else{
         return null;
     }
+    // Change z-index mobile nav
+    $('.navbar-mobile').addClass('navbar-mobile--open-sidebar');
 }
 
 $('[data-sidebar]').on('click', function( event ){
     event.preventDefault();
+    
+    if( $('body').hasClass('open-navbar') ){
+        closeNavbarOnClick();
+    }
+    
     openSidebarOnClick( $(this) );
 });
 
 $('.sidebar .sidebar__close').on('click', function(){
     $(this).closest('.sidebar').removeClass('sidebar--active');
     $('.overlay').addClass('overlay--disable');
-    $('body').removeClass('hidden')
+    $('body').removeClass('hidden');
+    $('body').removeClass('open-sidebar');
+
+    // Change z-index mobile nav
+    $('.navbar-mobile').removeClass('navbar-mobile--open-sidebar');
 });
 
 // Cобытие клика по веб-документу
@@ -29,9 +42,14 @@ $(document).on('mouseup', function (e){
     let sidebarActive = $(".sidebar.sidebar--active"); // элемент
     if (!sidebarActive.is(e.target) // клик был не по блоку
           && sidebarActive.has(e.target).length === 0 // и не по его дочерним элементам
+          && $('body').hasClass('open-sidebar')
     ) { 
         sidebarActive.removeClass('sidebar--active');
         $('.overlay').addClass('overlay--disable');
-        $('body').removeClass('hidden')
+        $('body').removeClass('hidden');
+        $('body').removeClass('open-sidebar');
+        
+        // Change z-index mobile nav
+        $('.navbar-mobile').removeClass('navbar-mobile--open-sidebar');
     }
 });
